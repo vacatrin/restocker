67418faa79cafc943212acf19678e8dc4befa516
Victor A <victor.acatrinei@gmail.com>
2023-06-01 00:47:01 +0300

    Create main class for spring boot application

---
+++ src/main/java/newPackage/MainSpringTutorial.java
@@ -0,0 +1,15 @@
+package newPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainSpringTutorial {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MainSpringTutorial.class, args);
+        System.out.println("spring boot");
+    }
+
+}
